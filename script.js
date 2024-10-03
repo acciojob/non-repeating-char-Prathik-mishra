4528447288bd4352aa6cbffcdf137923769824b5
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-10-03 15:38:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+ const charferqcount = {};
+	for(let char of str){
+		if(charferqcount[char]){
+			charferqcount += 1;
+		}else{
+			charferqcount[char] = 1;
+		}
+	}
+	for(let char of str){
+		if(charferqcount[char] === 1){
+			return char;
+		}
+		
+		}
+	return null;
+	}
+
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
